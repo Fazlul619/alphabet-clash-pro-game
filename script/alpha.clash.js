88254954e389aa666317390b10576970e514e0ec
Fazlul Alam <fazlulalam619@gmail.com>
2024-02-16 02:03:24 +0600

    Complete Game

--- script/alpha.clash.js
+++ script/alpha.clash.js
@@ -1,11 +1,30 @@
-// function play() {
-//   // step-1 hide screen
-//   const homeSection = document.getElementById("home-screen");
-//   homeSection.classList.add("hidden");
-//   //   show playground
-//   const PlayGroundSection = document.getElementById("play-ground");
-//   PlayGroundSection.classList.remove("hidden");
-// }
+function handleKeyboardButtonPress(event) {
+  const playerPressed = event.key;
+  // stop the game
+  if (playerPressed === "Escape") {
+    gameOver();
+  }
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectAlphabet = currentAlphabet.toLowerCase();
+  if (playerPressed === expectAlphabet) {
+    const currentScore = getTextElementValueById("current-score");
+    const updateScore = currentScore + 1;
+    setTextElementValueById("current-score", updateScore);
+
+    removeBackgroundColorById(expectAlphabet);
+    continueGame();
+  } else {
+    const currentLife = getTextElementValueById("current-life");
+    const updatedLife = currentLife - 1;
+    setTextElementValueById("current-life", updatedLife);
+    if (updatedLife === 0) {
+      gameOver();
+    }
+  }
+}
+document.addEventListener("keyup", handleKeyboardButtonPress);
+
 function continueGame() {
   // step-1 generate a random alphabet
   const alphabet = getARandomAlphabet();
@@ -17,6 +36,20 @@ function continueGame() {
 }
 function play() {
   hideElementById("home-screen");
+  hideElementById("final-score");
   showElementById("play-ground");
+  // reset function
+  setTextElementValueById("current-life", 5);
+  setTextElementValueById("current-score", 0);
   continueGame();
+}
+function gameOver() {
+  hideElementById("play-ground");
+  showElementById("final-score");
+  // update
+  const lastScore = getTextElementValueById("current-score");
+  setTextElementValueById("last-score", lastScore);
+  // clear the last selected
+  const currentAlphabet = getElementTextById("current-alphabet");
+  removeBackgroundColorById(currentAlphabet);
 }
